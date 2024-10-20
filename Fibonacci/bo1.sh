f47ddfa01d102a785d8a3ac4a227ab3e9f1d7ef3
HarolBarajas <fabianbap32@gmail.com>
2024-10-20 07:20:38 +0000

    Add files via upload

---
+++ Fibonacci/bo1.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+echo Burbuja Optimizada1 >> burbuja_op1.txt
+gcc burbuja_op_1.c tiempo.c tiempo.h -o bo1
+./bo1 1000 numeros10millones.txt >> burbuja_op1.txt
+./bo1 2000 numeros10millones.txt >> burbuja_op1.txt
+./bo1 3000 numeros10millones.txt >> burbuja_op1.txt
+./bo1 4000 numeros10millones.txt >> burbuja_op1.txt
+./bo1 5000 numeros10millones.txt >> burbuja_op1.txt
+./bo1 8000 numeros10millones.txt >> burbuja_op1.txt
+./bo1 10000 numeros10millones.txt >> burbuja_op1.txt
+./bo1 50000 numeros10millones.txt >> burbuja_op1.txt
+./bo1 100000 numeros10millones.txt >> burbuja_op1.txt
+./bo1 150000 numeros10millones.txt >> burbuja_op1.txt
+./bo1 200000 numeros10millones.txt >> burbuja_op1.txt
+./bo1 300000 numeros10millones.txt >> burbuja_op1.txt
+./bo1 400000 numeros10millones.txt >> burbuja_op1.txt
+./bo1 500000 numeros10millones.txt >> burbuja_op1.txt
+./bo1 600000 numeros10millones.txt >> burbuja_op1.txt
+./bo1 700000 numeros10millones.txt >> burbuja_op1.txt
+./bo1 800000 numeros10millones.txt >> burbuja_op1.txt
+./bo1 900000 numeros10millones.txt >> burbuja_op1.txt
+./bo1 1000000 numeros10millones.txt >> burbuja_op1.txt
+./bo1 1500000 numeros10millones.txt >> burbuja_op1.txt
+./bo1 2000000 numeros10millones.txt >> burbuja_op1.txt
