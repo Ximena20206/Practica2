2eaa51d6351f7418064ce95e0f93b4421e89d839
HarolBarajas <fabianbap32@gmail.com>
2024-10-21 03:22:19 +0000

    Add files via upload

---
+++ Fibonacci/fib.sh
@@ -0,0 +1,224 @@
+#!/bin/bash
+
+echo Fibonacci 1M >> fibo.txt
+gcc fibonacci.c tiempo.c tiempo.h -o fibonacci
+
+./fibonacci 1000000 10millones.txt 322486 >> fibo.txt
+./fibonacci 1000000 10millones.txt 14700764 >> fibo.txt
+./fibonacci 1000000 10millones.txt 3128036 >> fibo.txt
+./fibonacci 1000000 10millones.txt 6337399 >> fibo.txt
+./fibonacci 1000000 10millones.txt 61396 >> fibo.txt
+./fibonacci 1000000 10millones.txt 10393545 >> fibo.txt
+./fibonacci 1000000 10millones.txt 2147445644 >> fibo.txt
+./fibonacci 1000000 10millones.txt 1295390003 >> fibo.txt
+./fibonacci 1000000 10millones.txt 450057883 >> fibo.txt
+./fibonacci 1000000 10millones.txt 187645041 >> fibo.txt
+./fibonacci 1000000 10millones.txt 1980098116 >> fibo.txt
+./fibonacci 1000000 10millones.txt 152503 >> fibo.txt
+./fibonacci 1000000 10millones.txt 5000 >> fibo.txt
+./fibonacci 1000000 10millones.txt 1493283650 >> fibo.txt
+./fibonacci 1000000 10millones.txt 214826 >> fibo.txt
+./fibonacci 1000000 10millones.txt 1843349527 >> fibo.txt
+./fibonacci 1000000 10millones.txt 1360839354 >> fibo.txt
+./fibonacci 1000000 10millones.txt 2109248666 >> fibo.txt
+./fibonacci 1000000 10millones.txt 2147470852 >> fibo.txt
+./fibonacci 1000000 10millones.txt 0 >> fibo.txt
+
+
+echo Fibonacci 2M >> fibo.txt
+./fibonacci 2000000 10millones.txt 322486 >> fibo.txt
+./fibonacci 2000000 10millones.txt 14700764 >> fibo.txt
+./fibonacci 2000000 10millones.txt 3128036 >> fibo.txt
+./fibonacci 2000000 10millones.txt 6337399 >> fibo.txt
+./fibonacci 2000000 10millones.txt 61396 >> fibo.txt
+./fibonacci 2000000 10millones.txt 10393545 >> fibo.txt
+./fibonacci 2000000 10millones.txt 2147445644 >> fibo.txt
+./fibonacci 2000000 10millones.txt 1295390003 >> fibo.txt
+./fibonacci 2000000 10millones.txt 450057883 >> fibo.txt
+./fibonacci 2000000 10millones.txt 187645041 >> fibo.txt
+./fibonacci 2000000 10millones.txt 1980098116 >> fibo.txt
+./fibonacci 2000000 10millones.txt 152503 >> fibo.txt
+./fibonacci 2000000 10millones.txt 5000 >> fibo.txt
+./fibonacci 2000000 10millones.txt 1493283650 >> fibo.txt
+./fibonacci 2000000 10millones.txt 214826 >> fibo.txt
+./fibonacci 2000000 10millones.txt 1843349527 >> fibo.txt
+./fibonacci 2000000 10millones.txt 1360839354 >> fibo.txt
+./fibonacci 2000000 10millones.txt 2109248666 >> fibo.txt
+./fibonacci 2000000 10millones.txt 2147470852 >> fibo.txt
+./fibonacci 2000000 10millones.txt 0 >> fibo.txt
+
+echo Fibonacci 3M >> fibo.txt
+./fibonacci 3000000 10millones.txt 322486 >> fibo.txt
+./fibonacci 3000000 10millones.txt 14700764 >> fibo.txt
+./fibonacci 3000000 10millones.txt 3128036 >> fibo.txt
+./fibonacci 3000000 10millones.txt 6337399 >> fibo.txt
+./fibonacci 3000000 10millones.txt 61396 >> fibo.txt
+./fibonacci 3000000 10millones.txt 10393545 >> fibo.txt
+./fibonacci 3000000 10millones.txt 2147445644 >> fibo.txt
+./fibonacci 3000000 10millones.txt 1295390003 >> fibo.txt
+./fibonacci 3000000 10millones.txt 450057883 >> fibo.txt
+./fibonacci 3000000 10millones.txt 187645041 >> fibo.txt
+./fibonacci 3000000 10millones.txt 1980098116 >> fibo.txt
+./fibonacci 3000000 10millones.txt 152503 >> fibo.txt
+./fibonacci 3000000 10millones.txt 5000 >> fibo.txt
+./fibonacci 3000000 10millones.txt 1493283650 >> fibo.txt
+./fibonacci 3000000 10millones.txt 214826 >> fibo.txt
+./fibonacci 3000000 10millones.txt 1843349527 >> fibo.txt
+./fibonacci 3000000 10millones.txt 1360839354 >> fibo.txt
+./fibonacci 3000000 10millones.txt 2109248666 >> fibo.txt
+./fibonacci 3000000 10millones.txt 2147470852 >> fibo.txt
+./fibonacci 3000000 10millones.txt 0 >> fibo.txt
+
+echo Fibonacci 4M >> fibo.txt
+./fibonacci 4000000 10millones.txt 322486 >> fibo.txt
+./fibonacci 4000000 10millones.txt 14700764 >> fibo.txt
+./fibonacci 4000000 10millones.txt 3128036 >> fibo.txt
+./fibonacci 4000000 10millones.txt 6337399 >> fibo.txt
+./fibonacci 4000000 10millones.txt 61396 >> fibo.txt
+./fibonacci 4000000 10millones.txt 10393545 >> fibo.txt
+./fibonacci 4000000 10millones.txt 2147445644 >> fibo.txt
+./fibonacci 4000000 10millones.txt 1295390003 >> fibo.txt
+./fibonacci 4000000 10millones.txt 450057883 >> fibo.txt
+./fibonacci 4000000 10millones.txt 187645041 >> fibo.txt
+./fibonacci 4000000 10millones.txt 1980098116 >> fibo.txt
+./fibonacci 4000000 10millones.txt 152503 >> fibo.txt
+./fibonacci 4000000 10millones.txt 5000 >> fibo.txt
+./fibonacci 4000000 10millones.txt 1493283650 >> fibo.txt
+./fibonacci 4000000 10millones.txt 214826 >> fibo.txt
+./fibonacci 4000000 10millones.txt 1843349527 >> fibo.txt
+./fibonacci 4000000 10millones.txt 1360839354 >> fibo.txt
+./fibonacci 4000000 10millones.txt 2109248666 >> fibo.txt
+./fibonacci 4000000 10millones.txt 2147470852 >> fibo.txt
+./fibonacci 4000000 10millones.txt 0 >> fibo.txt
+
+echo Fibonacci 5M >> fibo.txt
+./fibonacci 5000000 10millones.txt 322486 >> fibo.txt
+./fibonacci 5000000 10millones.txt 14700764 >> fibo.txt
+./fibonacci 5000000 10millones.txt 3128036 >> fibo.txt
+./fibonacci 5000000 10millones.txt 6337399 >> fibo.txt
+./fibonacci 5000000 10millones.txt 61396 >> fibo.txt
+./fibonacci 5000000 10millones.txt 10393545 >> fibo.txt
+./fibonacci 5000000 10millones.txt 2147445644 >> fibo.txt
+./fibonacci 5000000 10millones.txt 1295390003 >> fibo.txt
+./fibonacci 5000000 10millones.txt 450057883 >> fibo.txt
+./fibonacci 5000000 10millones.txt 187645041 >> fibo.txt
+./fibonacci 5000000 10millones.txt 1980098116 >> fibo.txt
+./fibonacci 5000000 10millones.txt 152503 >> fibo.txt
+./fibonacci 5000000 10millones.txt 5000 >> fibo.txt
+./fibonacci 5000000 10millones.txt 1493283650 >> fibo.txt
+./fibonacci 5000000 10millones.txt 214826 >> fibo.txt
+./fibonacci 5000000 10millones.txt 1843349527 >> fibo.txt
+./fibonacci 5000000 10millones.txt 1360839354 >> fibo.txt
+./fibonacci 5000000 10millones.txt 2109248666 >> fibo.txt
+./fibonacci 5000000 10millones.txt 2147470852 >> fibo.txt
+./fibonacci 5000000 10millones.txt 0 >> fibo.txt
+
+echo Fibonacci 6M >> fibo.txt
+./fibonacci 6000000 10millones.txt 322486 >> fibo.txt
+./fibonacci 6000000 10millones.txt 14700764 >> fibo.txt
+./fibonacci 6000000 10millones.txt 3128036 >> fibo.txt
+./fibonacci 6000000 10millones.txt 6337399 >> fibo.txt
+./fibonacci 6000000 10millones.txt 61396 >> fibo.txt
+./fibonacci 6000000 10millones.txt 10393545 >> fibo.txt
+./fibonacci 6000000 10millones.txt 2147445644 >> fibo.txt
+./fibonacci 6000000 10millones.txt 1295390003 >> fibo.txt
+./fibonacci 6000000 10millones.txt 450057883 >> fibo.txt
+./fibonacci 6000000 10millones.txt 187645041 >> fibo.txt
+./fibonacci 6000000 10millones.txt 1980098116 >> fibo.txt
+./fibonacci 6000000 10millones.txt 152503 >> fibo.txt
+./fibonacci 6000000 10millones.txt 5000 >> fibo.txt
+./fibonacci 6000000 10millones.txt 1493283650 >> fibo.txt
+./fibonacci 6000000 10millones.txt 214826 >> fibo.txt
+./fibonacci 6000000 10millones.txt 1843349527 >> fibo.txt
+./fibonacci 6000000 10millones.txt 1360839354 >> fibo.txt
+./fibonacci 6000000 10millones.txt 2109248666 >> fibo.txt
+./fibonacci 6000000 10millones.txt 2147470852 >> fibo.txt
+./fibonacci 6000000 10millones.txt 0 >> fibo.txt
+
+echo Fibonacci 7M >> fibo.txt
+./fibonacci 7000000 10millones.txt 322486 >> fibo.txt
+./fibonacci 7000000 10millones.txt 14700764 >> fibo.txt
+./fibonacci 7000000 10millones.txt 3128036 >> fibo.txt
+./fibonacci 7000000 10millones.txt 6337399 >> fibo.txt
+./fibonacci 7000000 10millones.txt 61396 >> fibo.txt
+./fibonacci 7000000 10millones.txt 10393545 >> fibo.txt
+./fibonacci 7000000 10millones.txt 2147445644 >> fibo.txt
+./fibonacci 7000000 10millones.txt 1295390003 >> fibo.txt
+./fibonacci 7000000 10millones.txt 450057883 >> fibo.txt
+./fibonacci 7000000 10millones.txt 187645041 >> fibo.txt
+./fibonacci 7000000 10millones.txt 1980098116 >> fibo.txt
+./fibonacci 7000000 10millones.txt 152503 >> fibo.txt
+./fibonacci 7000000 10millones.txt 5000 >> fibo.txt
+./fibonacci 7000000 10millones.txt 1493283650 >> fibo.txt
+./fibonacci 7000000 10millones.txt 214826 >> fibo.txt
+./fibonacci 7000000 10millones.txt 1843349527 >> fibo.txt
+./fibonacci 7000000 10millones.txt 1360839354 >> fibo.txt
+./fibonacci 7000000 10millones.txt 2109248666 >> fibo.txt
+./fibonacci 7000000 10millones.txt 2147470852 >> fibo.txt
+./fibonacci 7000000 10millones.txt 0 >> fibo.txt
+
+echo Fibonacci 8M >> fibo.txt
+./fibonacci 8000000 10millones.txt 322486 >> fibo.txt
+./fibonacci 8000000 10millones.txt 14700764 >> fibo.txt
+./fibonacci 8000000 10millones.txt 3128036 >> fibo.txt
+./fibonacci 8000000 10millones.txt 6337399 >> fibo.txt
+./fibonacci 8000000 10millones.txt 61396 >> fibo.txt
+./fibonacci 8000000 10millones.txt 10393545 >> fibo.txt
+./fibonacci 8000000 10millones.txt 2147445644 >> fibo.txt
+./fibonacci 8000000 10millones.txt 1295390003 >> fibo.txt
+./fibonacci 8000000 10millones.txt 450057883 >> fibo.txt
+./fibonacci 8000000 10millones.txt 187645041 >> fibo.txt
+./fibonacci 8000000 10millones.txt 1980098116 >> fibo.txt
+./fibonacci 8000000 10millones.txt 152503 >> fibo.txt
+./fibonacci 8000000 10millones.txt 5000 >> fibo.txt
+./fibonacci 8000000 10millones.txt 1493283650 >> fibo.txt
+./fibonacci 8000000 10millones.txt 214826 >> fibo.txt
+./fibonacci 8000000 10millones.txt 1843349527 >> fibo.txt
+./fibonacci 8000000 10millones.txt 1360839354 >> fibo.txt
+./fibonacci 8000000 10millones.txt 2109248666 >> fibo.txt
+./fibonacci 8000000 10millones.txt 2147470852 >> fibo.txt
+./fibonacci 8000000 10millones.txt 0 >> fibo.txt
+
+echo Fibonacci 9M >> fibo.txt
+./fibonacci 9000000 10millones.txt 322486 >> fibo.txt
+./fibonacci 9000000 10millones.txt 14700764 >> fibo.txt
+./fibonacci 9000000 10millones.txt 3128036 >> fibo.txt
+./fibonacci 9000000 10millones.txt 6337399 >> fibo.txt
+./fibonacci 9000000 10millones.txt 61396 >> fibo.txt
+./fibonacci 9000000 10millones.txt 10393545 >> fibo.txt
+./fibonacci 9000000 10millones.txt 2147445644 >> fibo.txt
+./fibonacci 9000000 10millones.txt 1295390003 >> fibo.txt
+./fibonacci 9000000 10millones.txt 450057883 >> fibo.txt
+./fibonacci 9000000 10millones.txt 187645041 >> fibo.txt
+./fibonacci 9000000 10millones.txt 1980098116 >> fibo.txt
+./fibonacci 9000000 10millones.txt 152503 >> fibo.txt
+./fibonacci 9000000 10millones.txt 5000 >> fibo.txt
+./fibonacci 9000000 10millones.txt 1493283650 >> fibo.txt
+./fibonacci 9000000 10millones.txt 214826 >> fibo.txt
+./fibonacci 9000000 10millones.txt 1843349527 >> fibo.txt
+./fibonacci 9000000 10millones.txt 1360839354 >> fibo.txt
+./fibonacci 9000000 10millones.txt 2109248666 >> fibo.txt
+./fibonacci 9000000 10millones.txt 2147470852 >> fibo.txt
+./fibonacci 9000000 10millones.txt 0 >> fibo.txt
+
+echo Fibonacci 10M >> fibo.txt
+./fibonacci 10000000 10millones.txt 322486 >> fibo.txt
+./fibonacci 10000000 10millones.txt 14700764 >> fibo.txt
+./fibonacci 10000000 10millones.txt 3128036 >> fibo.txt
+./fibonacci 10000000 10millones.txt 6337399 >> fibo.txt
+./fibonacci 10000000 10millones.txt 61396 >> fibo.txt
+./fibonacci 10000000 10millones.txt 10393545 >> fibo.txt
+./fibonacci 10000000 10millones.txt 2147445644 >> fibo.txt
+./fibonacci 10000000 10millones.txt 1295390003 >> fibo.txt
+./fibonacci 10000000 10millones.txt 450057883 >> fibo.txt
+./fibonacci 10000000 10millones.txt 187645041 >> fibo.txt
+./fibonacci 10000000 10millones.txt 1980098116 >> fibo.txt
+./fibonacci 10000000 10millones.txt 152503 >> fibo.txt
+./fibonacci 10000000 10millones.txt 5000 >> fibo.txt
+./fibonacci 10000000 10millones.txt 1493283650 >> fibo.txt
+./fibonacci 10000000 10millones.txt 214826 >> fibo.txt
+./fibonacci 10000000 10millones.txt 1843349527 >> fibo.txt
+./fibonacci 10000000 10millones.txt 1360839354 >> fibo.txt
+./fibonacci 10000000 10millones.txt 2109248666 >> fibo.txt
+./fibonacci 10000000 10millones.txt 2147470852 >> fibo.txt
+./fibonacci 10000000 10millones.txt 0 >> fibo.txt
