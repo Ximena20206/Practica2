9cae9d215c311da29db52c43639e221dc8a11b58
HarolBarajas <fabianbap32@gmail.com>
2024-10-21 05:25:44 +0000

    Add files via upload

---
+++ Fibonacci/fibhi.sh
@@ -0,0 +1,224 @@
+#!/bin/bash
+
+echo FibonacciH 1M >> fibohi.txt
+gcc fibonaccihilos.c tiempo.c tiempo.h -o fhilos
+
+./fhilos 1000000 10millones.txt 322486 >> fibohi.txt
+./fhilos 1000000 10millones.txt 14700764 >> fibohi.txt
+./fhilos 1000000 10millones.txt 3128036 >> fibohi.txt
+./fhilos 1000000 10millones.txt 6337399 >> fibohi.txt
+./fhilos 1000000 10millones.txt 61396 >> fibohi.txt
+./fhilos 1000000 10millones.txt 10393545 >> fibohi.txt
+./fhilos 1000000 10millones.txt 2147445644 >> fibohi.txt
+./fhilos 1000000 10millones.txt 1295390003 >> fibohi.txt
+./fhilos 1000000 10millones.txt 450057883 >> fibohi.txt
+./fhilos 1000000 10millones.txt 187645041 >> fibohi.txt
+./fhilos 1000000 10millones.txt 1980098116 >> fibohi.txt
+./fhilos 1000000 10millones.txt 152503 >> fibohi.txt
+./fhilos 1000000 10millones.txt 5000 >> fibohi.txt
+./fhilos 1000000 10millones.txt 1493283650 >> fibohi.txt
+./fhilos 1000000 10millones.txt 214826 >> fibohi.txt
+./fhilos 1000000 10millones.txt 1843349527 >> fibohi.txt
+./fhilos 1000000 10millones.txt 1360839354 >> fibohi.txt
+./fhilos 1000000 10millones.txt 2109248666 >> fibohi.txt
+./fhilos 1000000 10millones.txt 2147470852 >> fibohi.txt
+./fhilos 1000000 10millones.txt 0 >> fibohi.txt
+
+
+echo FibonacciH 2M >> fibohi.txt
+./fhilos 2000000 10millones.txt 322486 >> fibohi.txt
+./fhilos 2000000 10millones.txt 14700764 >> fibohi.txt
+./fhilos 2000000 10millones.txt 3128036 >> fibohi.txt
+./fhilos 2000000 10millones.txt 6337399 >> fibohi.txt
+./fhilos 2000000 10millones.txt 61396 >> fibohi.txt
+./fhilos 2000000 10millones.txt 10393545 >> fibohi.txt
+./fhilos 2000000 10millones.txt 2147445644 >> fibohi.txt
+./fhilos 2000000 10millones.txt 1295390003 >> fibohi.txt
+./fhilos 2000000 10millones.txt 450057883 >> fibohi.txt
+./fhilos 2000000 10millones.txt 187645041 >> fibohi.txt
+./fhilos 2000000 10millones.txt 1980098116 >> fibohi.txt
+./fhilos 2000000 10millones.txt 152503 >> fibohi.txt
+./fhilos 2000000 10millones.txt 5000 >> fibohi.txt
+./fhilos 2000000 10millones.txt 1493283650 >> fibohi.txt
+./fhilos 2000000 10millones.txt 214826 >> fibohi.txt
+./fhilos 2000000 10millones.txt 1843349527 >> fibohi.txt
+./fhilos 2000000 10millones.txt 1360839354 >> fibohi.txt
+./fhilos 2000000 10millones.txt 2109248666 >> fibohi.txt
+./fhilos 2000000 10millones.txt 2147470852 >> fibohi.txt
+./fhilos 2000000 10millones.txt 0 >> fibohi.txt
+
+echo FibonacciH 3M >> fibohi.txt
+./fhilos 3000000 10millones.txt 322486 >> fibohi.txt
+./fhilos 3000000 10millones.txt 14700764 >> fibohi.txt
+./fhilos 3000000 10millones.txt 3128036 >> fibohi.txt
+./fhilos 3000000 10millones.txt 6337399 >> fibohi.txt
+./fhilos 3000000 10millones.txt 61396 >> fibohi.txt
+./fhilos 3000000 10millones.txt 10393545 >> fibohi.txt
+./fhilos 3000000 10millones.txt 2147445644 >> fibohi.txt
+./fhilos 3000000 10millones.txt 1295390003 >> fibohi.txt
+./fhilos 3000000 10millones.txt 450057883 >> fibohi.txt
+./fhilos 3000000 10millones.txt 187645041 >> fibohi.txt
+./fhilos 3000000 10millones.txt 1980098116 >> fibohi.txt
+./fhilos 3000000 10millones.txt 152503 >> fibohi.txt
+./fhilos 3000000 10millones.txt 5000 >> fibohi.txt
+./fhilos 3000000 10millones.txt 1493283650 >> fibohi.txt
+./fhilos 3000000 10millones.txt 214826 >> fibohi.txt
+./fhilos 3000000 10millones.txt 1843349527 >> fibohi.txt
+./fhilos 3000000 10millones.txt 1360839354 >> fibohi.txt
+./fhilos 3000000 10millones.txt 2109248666 >> fibohi.txt
+./fhilos 3000000 10millones.txt 2147470852 >> fibohi.txt
+./fhilos 3000000 10millones.txt 0 >> fibohi.txt
+
+echo FibonacciH 4M >> fibohi.txt
+./fhilos 4000000 10millones.txt 322486 >> fibohi.txt
+./fhilos 4000000 10millones.txt 14700764 >> fibohi.txt
+./fhilos 4000000 10millones.txt 3128036 >> fibohi.txt
+./fhilos 4000000 10millones.txt 6337399 >> fibohi.txt
+./fhilos 4000000 10millones.txt 61396 >> fibohi.txt
+./fhilos 4000000 10millones.txt 10393545 >> fibohi.txt
+./fhilos 4000000 10millones.txt 2147445644 >> fibohi.txt
+./fhilos 4000000 10millones.txt 1295390003 >> fibohi.txt
+./fhilos 4000000 10millones.txt 450057883 >> fibohi.txt
+./fhilos 4000000 10millones.txt 187645041 >> fibohi.txt
+./fhilos 4000000 10millones.txt 1980098116 >> fibohi.txt
+./fhilos 4000000 10millones.txt 152503 >> fibohi.txt
+./fhilos 4000000 10millones.txt 5000 >> fibohi.txt
+./fhilos 4000000 10millones.txt 1493283650 >> fibohi.txt
+./fhilos 4000000 10millones.txt 214826 >> fibohi.txt
+./fhilos 4000000 10millones.txt 1843349527 >> fibohi.txt
+./fhilos 4000000 10millones.txt 1360839354 >> fibohi.txt
+./fhilos 4000000 10millones.txt 2109248666 >> fibohi.txt
+./fhilos 4000000 10millones.txt 2147470852 >> fibohi.txt
+./fhilos 4000000 10millones.txt 0 >> fibohi.txt
+
+echo FibonacciH 5M >> fibohi.txt
+./fhilos 5000000 10millones.txt 322486 >> fibohi.txt
+./fhilos 5000000 10millones.txt 14700764 >> fibohi.txt
+./fhilos 5000000 10millones.txt 3128036 >> fibohi.txt
+./fhilos 5000000 10millones.txt 6337399 >> fibohi.txt
+./fhilos 5000000 10millones.txt 61396 >> fibohi.txt
+./fhilos 5000000 10millones.txt 10393545 >> fibohi.txt
+./fhilos 5000000 10millones.txt 2147445644 >> fibohi.txt
+./fhilos 5000000 10millones.txt 1295390003 >> fibohi.txt
+./fhilos 5000000 10millones.txt 450057883 >> fibohi.txt
+./fhilos 5000000 10millones.txt 187645041 >> fibohi.txt
+./fhilos 5000000 10millones.txt 1980098116 >> fibohi.txt
+./fhilos 5000000 10millones.txt 152503 >> fibohi.txt
+./fhilos 5000000 10millones.txt 5000 >> fibohi.txt
+./fhilos 5000000 10millones.txt 1493283650 >> fibohi.txt
+./fhilos 5000000 10millones.txt 214826 >> fibohi.txt
+./fhilos 5000000 10millones.txt 1843349527 >> fibohi.txt
+./fhilos 5000000 10millones.txt 1360839354 >> fibohi.txt
+./fhilos 5000000 10millones.txt 2109248666 >> fibohi.txt
+./fhilos 5000000 10millones.txt 2147470852 >> fibohi.txt
+./fhilos 5000000 10millones.txt 0 >> fibohi.txt
+
+echo FibonacciH 6M >> fibohi.txt
+./fhilos 6000000 10millones.txt 322486 >> fibohi.txt
+./fhilos 6000000 10millones.txt 14700764 >> fibohi.txt
+./fhilos 6000000 10millones.txt 3128036 >> fibohi.txt
+./fhilos 6000000 10millones.txt 6337399 >> fibohi.txt
+./fhilos 6000000 10millones.txt 61396 >> fibohi.txt
+./fhilos 6000000 10millones.txt 10393545 >> fibohi.txt
+./fhilos 6000000 10millones.txt 2147445644 >> fibohi.txt
+./fhilos 6000000 10millones.txt 1295390003 >> fibohi.txt
+./fhilos 6000000 10millones.txt 450057883 >> fibohi.txt
+./fhilos 6000000 10millones.txt 187645041 >> fibohi.txt
+./fhilos 6000000 10millones.txt 1980098116 >> fibohi.txt
+./fhilos 6000000 10millones.txt 152503 >> fibohi.txt
+./fhilos 6000000 10millones.txt 5000 >> fibohi.txt
+./fhilos 6000000 10millones.txt 1493283650 >> fibohi.txt
+./fhilos 6000000 10millones.txt 214826 >> fibohi.txt
+./fhilos 6000000 10millones.txt 1843349527 >> fibohi.txt
+./fhilos 6000000 10millones.txt 1360839354 >> fibohi.txt
+./fhilos 6000000 10millones.txt 2109248666 >> fibohi.txt
+./fhilos 6000000 10millones.txt 2147470852 >> fibohi.txt
+./fhilos 6000000 10millones.txt 0 >> fibohi.txt
+
+echo FibonacciH 7M >> fibohi.txt
+./fhilos 7000000 10millones.txt 322486 >> fibohi.txt
+./fhilos 7000000 10millones.txt 14700764 >> fibohi.txt
+./fhilos 7000000 10millones.txt 3128036 >> fibohi.txt
+./fhilos 7000000 10millones.txt 6337399 >> fibohi.txt
+./fhilos 7000000 10millones.txt 61396 >> fibohi.txt
+./fhilos 7000000 10millones.txt 10393545 >> fibohi.txt
+./fhilos 7000000 10millones.txt 2147445644 >> fibohi.txt
+./fhilos 7000000 10millones.txt 1295390003 >> fibohi.txt
+./fhilos 7000000 10millones.txt 450057883 >> fibohi.txt
+./fhilos 7000000 10millones.txt 187645041 >> fibohi.txt
+./fhilos 7000000 10millones.txt 1980098116 >> fibohi.txt
+./fhilos 7000000 10millones.txt 152503 >> fibohi.txt
+./fhilos 7000000 10millones.txt 5000 >> fibohi.txt
+./fhilos 7000000 10millones.txt 1493283650 >> fibohi.txt
+./fhilos 7000000 10millones.txt 214826 >> fibohi.txt
+./fhilos 7000000 10millones.txt 1843349527 >> fibohi.txt
+./fhilos 7000000 10millones.txt 1360839354 >> fibohi.txt
+./fhilos 7000000 10millones.txt 2109248666 >> fibohi.txt
+./fhilos 7000000 10millones.txt 2147470852 >> fibohi.txt
+./fhilos 7000000 10millones.txt 0 >> fibohi.txt
+
+echo FibonacciH 8M >> fibohi.txt
+./fhilos 8000000 10millones.txt 322486 >> fibohi.txt
+./fhilos 8000000 10millones.txt 14700764 >> fibohi.txt
+./fhilos 8000000 10millones.txt 3128036 >> fibohi.txt
+./fhilos 8000000 10millones.txt 6337399 >> fibohi.txt
+./fhilos 8000000 10millones.txt 61396 >> fibohi.txt
+./fhilos 8000000 10millones.txt 10393545 >> fibohi.txt
+./fhilos 8000000 10millones.txt 2147445644 >> fibohi.txt
+./fhilos 8000000 10millones.txt 1295390003 >> fibohi.txt
+./fhilos 8000000 10millones.txt 450057883 >> fibohi.txt
+./fhilos 8000000 10millones.txt 187645041 >> fibohi.txt
+./fhilos 8000000 10millones.txt 1980098116 >> fibohi.txt
+./fhilos 8000000 10millones.txt 152503 >> fibohi.txt
+./fhilos 8000000 10millones.txt 5000 >> fibohi.txt
+./fhilos 8000000 10millones.txt 1493283650 >> fibohi.txt
+./fhilos 8000000 10millones.txt 214826 >> fibohi.txt
+./fhilos 8000000 10millones.txt 1843349527 >> fibohi.txt
+./fhilos 8000000 10millones.txt 1360839354 >> fibohi.txt
+./fhilos 8000000 10millones.txt 2109248666 >> fibohi.txt
+./fhilos 8000000 10millones.txt 2147470852 >> fibohi.txt
+./fhilos 8000000 10millones.txt 0 >> fibohi.txt
+
+echo FibonacciH 9M >> fibohi.txt
+./fhilos 9000000 10millones.txt 322486 >> fibohi.txt
+./fhilos 9000000 10millones.txt 14700764 >> fibohi.txt
+./fhilos 9000000 10millones.txt 3128036 >> fibohi.txt
+./fhilos 9000000 10millones.txt 6337399 >> fibohi.txt
+./fhilos 9000000 10millones.txt 61396 >> fibohi.txt
+./fhilos 9000000 10millones.txt 10393545 >> fibohi.txt
+./fhilos 9000000 10millones.txt 2147445644 >> fibohi.txt
+./fhilos 9000000 10millones.txt 1295390003 >> fibohi.txt
+./fhilos 9000000 10millones.txt 450057883 >> fibohi.txt
+./fhilos 9000000 10millones.txt 187645041 >> fibohi.txt
+./fhilos 9000000 10millones.txt 1980098116 >> fibohi.txt
+./fhilos 9000000 10millones.txt 152503 >> fibohi.txt
+./fhilos 9000000 10millones.txt 5000 >> fibohi.txt
+./fhilos 9000000 10millones.txt 1493283650 >> fibohi.txt
+./fhilos 9000000 10millones.txt 214826 >> fibohi.txt
+./fhilos 9000000 10millones.txt 1843349527 >> fibohi.txt
+./fhilos 9000000 10millones.txt 1360839354 >> fibohi.txt
+./fhilos 9000000 10millones.txt 2109248666 >> fibohi.txt
+./fhilos 9000000 10millones.txt 2147470852 >> fibohi.txt
+./fhilos 9000000 10millones.txt 0 >> fibohi.txt
+
+echo FibonacciH 10M >> fibohi.txt
+./fhilos 10000000 10millones.txt 322486 >> fibohi.txt
+./fhilos 10000000 10millones.txt 14700764 >> fibohi.txt
+./fhilos 10000000 10millones.txt 3128036 >> fibohi.txt
+./fhilos 10000000 10millones.txt 6337399 >> fibohi.txt
+./fhilos 10000000 10millones.txt 61396 >> fibohi.txt
+./fhilos 10000000 10millones.txt 10393545 >> fibohi.txt
+./fhilos 10000000 10millones.txt 2147445644 >> fibohi.txt
+./fhilos 10000000 10millones.txt 1295390003 >> fibohi.txt
+./fhilos 10000000 10millones.txt 450057883 >> fibohi.txt
+./fhilos 10000000 10millones.txt 187645041 >> fibohi.txt
+./fhilos 10000000 10millones.txt 1980098116 >> fibohi.txt
+./fhilos 10000000 10millones.txt 152503 >> fibohi.txt
+./fhilos 10000000 10millones.txt 5000 >> fibohi.txt
+./fhilos 10000000 10millones.txt 1493283650 >> fibohi.txt
+./fhilos 10000000 10millones.txt 214826 >> fibohi.txt
+./fhilos 10000000 10millones.txt 1843349527 >> fibohi.txt
+./fhilos 10000000 10millones.txt 1360839354 >> fibohi.txt
+./fhilos 10000000 10millones.txt 2109248666 >> fibohi.txt
+./fhilos 10000000 10millones.txt 2147470852 >> fibohi.txt
+./fhilos 10000000 10millones.txt 0 >> fibohi.txt
